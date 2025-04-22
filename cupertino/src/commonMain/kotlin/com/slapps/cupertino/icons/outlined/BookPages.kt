d6477557dd2398e4c79337d848b062cfa83a2f4e
Simone Bianchin <simone.bianchin99@gmail.com>
2025-04-22 12:52:55 +0200

    chore(cupertino): updated submodule

---
+++ cupertino/src/commonMain/kotlin/com/slapps/cupertino/icons/outlined/BookPages.kt
@@ -0,0 +1,151 @@
+/*
+* Converted using https://composables.com/svgtocompose
+*/
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.group
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.slapps.cupertino.icons.CupertinoIcons
+
+public val CupertinoIcons.Outlined.BookPages: ImageVector
+    get() {
+        if (_bookPages != null) {
+            return _bookPages!!
+        }
+        _bookPages =
+            ImageVector
+                .Builder(
+                    name = "Book.pages",
+                    defaultWidth = 42.3761.dp,
+                    defaultHeight = 49.6551.dp,
+                    viewportWidth = 42.3761f,
+                    viewportHeight = 49.6551f,
+                ).apply {
+                    group {
+                        path(
+                            fill = SolidColor(Color.Black),
+                            fillAlpha = 0f,
+                            stroke = null,
+                            strokeAlpha = 0f,
+                            strokeLineWidth = 1.0f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Miter,
+                            strokeLineMiter = 1.0f,
+                            pathFillType = PathFillType.NonZero,
+                        ) {
+                            moveTo(0f, 0f)
+                            horizontalLineTo(42.3761f)
+                            verticalLineTo(49.6551f)
+                            horizontalLineTo(0f)
+                            verticalLineTo(0f)
+                            close()
+                        }
+                        path(
+                            fill = SolidColor(Color(0xFF000000)),
+                            fillAlpha = 1.0f,
+                            stroke = null,
+                            strokeAlpha = 1.0f,
+                            strokeLineWidth = 1.0f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Miter,
+                            strokeLineMiter = 1.0f,
+                            pathFillType = PathFillType.NonZero,
+                        ) {
+                            moveTo(39.2082f, 12.1498f)
+                            lineTo(39.2082f, 41.0873f)
+                            curveTo(39.20820f, 45.32750f, 37.0750f, 47.46420f, 32.89680f, 47.46420f)
+                            lineTo(19.216f, 47.4642f)
+                            curveTo(22.49290f, 46.97510f, 25.85280f, 46.27780f, 29.41790f, 45.27280f)
+                            curveTo(33.54450f, 44.06970f, 35.43470f, 41.35490f, 35.43470f, 36.63890f)
+                            lineTo(35.4347f, 8.09081f)
+                            curveTo(35.43470f, 7.36590f, 35.37550f, 6.68340f, 35.25840f, 6.04860f)
+                            curveTo(37.8790f, 6.75630f, 39.20820f, 8.80270f, 39.20820f, 12.14980f)
+                            close()
+                        }
+                        path(
+                            fill = SolidColor(Color(0xFF000000)),
+                            fillAlpha = 1.0f,
+                            stroke = null,
+                            strokeAlpha = 1.0f,
+                            strokeLineWidth = 1.0f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Miter,
+                            strokeLineMiter = 1.0f,
+                            pathFillType = PathFillType.NonZero,
+                        ) {
+                            moveTo(2.79687f, 40.4537f)
+                            curveTo(2.79690f, 43.98570f, 4.99410f, 45.90170f, 8.79370f, 45.66930f)
+                            curveTo(15.35970f, 45.31070f, 21.53670f, 44.56810f, 28.6390f, 42.58060f)
+                            curveTo(31.56720f, 41.73960f, 32.63440f, 40.10250f, 32.63440f, 36.63890f)
+                            lineTo(32.6344f, 8.09081f)
+                            curveTo(32.63440f, 4.02130f, 30.35470f, 2.10210f, 26.450f, 3.02320f)
+                            curveTo(21.0480f, 4.26070f, 15.02580f, 5.26890f, 8.17730f, 5.68610f)
+                            curveTo(4.87620f, 5.90250f, 2.79690f, 7.7240f, 2.79690f, 11.32950f)
+                            close()
+                            moveTo(6.46169f, 39.8205f)
+                            lineTo(6.46169f, 11.4131f)
+                            curveTo(6.46170f, 9.95520f, 7.38590f, 9.34940f, 8.71720f, 9.27480f)
+                            curveTo(15.11250f, 8.93020f, 21.02110f, 7.92560f, 26.36910f, 6.6970f)
+                            curveTo(28.00620f, 6.32560f, 28.9730f, 7.17870f, 28.9730f, 8.77830f)
+                            lineTo(28.973f, 36.5182f)
+                            curveTo(28.9730f, 38.16030f, 28.45550f, 38.92950f, 27.04650f, 39.30250f)
+                            curveTo(21.26010f, 40.9490f, 15.35150f, 41.66890f, 8.9480f, 42.0080f)
+                            curveTo(7.40f, 42.08260f, 6.46170f, 41.29350f, 6.46170f, 39.82050f)
+                            close()
+                        }
+                        path(
+                            fill = SolidColor(Color(0xFF000000)),
+                            fillAlpha = 1.0f,
+                            stroke = null,
+                            strokeAlpha = 1.0f,
+                            strokeLineWidth = 1.0f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Miter,
+                            strokeLineMiter = 1.0f,
+                            pathFillType = PathFillType.NonZero,
+                        ) {
+                            moveTo(10.7164f, 16.2416f)
+                            curveTo(16.07850f, 15.8830f, 20.38050f, 15.13180f, 24.75190f, 14.15130f)
+                            curveTo(25.6750f, 13.9420f, 25.89690f, 13.46810f, 25.89690f, 12.91070f)
+                            curveTo(25.89690f, 12.28060f, 25.4070f, 11.72170f, 24.5090f, 11.90450f)
+                            curveTo(20.35580f, 12.83340f, 16.06440f, 13.60060f, 10.70580f, 13.95210f)
+                            curveTo(9.90f, 14.01770f, 9.53430f, 14.51460f, 9.53430f, 15.13420f)
+                            curveTo(9.53430f, 15.78380f, 10.02070f, 16.28060f, 10.71640f, 16.24160f)
+                            close()
+                            moveTo(10.7164f, 22.3092f)
+                            curveTo(16.07850f, 21.94710f, 20.38050f, 21.18340f, 24.75190f, 20.21890f)
+                            curveTo(25.6750f, 20.00950f, 25.89690f, 19.53570f, 25.89690f, 18.97480f)
+                            curveTo(25.89690f, 18.34470f, 25.4070f, 17.78920f, 24.5090f, 17.97210f)
+                            curveTo(20.35580f, 18.9010f, 16.06440f, 19.66460f, 10.70580f, 20.01970f)
+                            curveTo(9.90f, 20.07830f, 9.53430f, 20.57870f, 9.53430f, 21.18220f)
+                            curveTo(9.53430f, 21.83880f, 10.02070f, 22.34470f, 10.71640f, 22.30920f)
+                            close()
+                            moveTo(10.7164f, 28.3732f)
+                            curveTo(16.07850f, 28.01460f, 20.38050f, 27.24750f, 24.75190f, 26.2830f)
+                            curveTo(25.6750f, 26.07360f, 25.89690f, 25.59980f, 25.89690f, 25.04240f)
+                            curveTo(25.89690f, 24.41230f, 25.4070f, 23.84980f, 24.5090f, 24.03610f)
+                            curveTo(20.35580f, 24.96150f, 16.06440f, 25.72870f, 10.70580f, 26.08380f)
+                            curveTo(9.90f, 26.14940f, 9.53430f, 26.64630f, 9.53430f, 27.24980f)
+                            curveTo(9.53430f, 27.89590f, 10.02070f, 28.41230f, 10.71640f, 28.37320f)
+                            close()
+                            moveTo(10.7164f, 34.4178f)
+                            curveTo(13.84220f, 34.2420f, 15.91130f, 33.93650f, 17.92930f, 33.6080f)
+                            curveTo(18.60190f, 33.48730f, 18.96050f, 32.95490f, 18.96050f, 32.4260f)
+                            curveTo(18.96050f, 31.81190f, 18.47420f, 31.21740f, 17.55310f, 31.38420f)
+                            curveTo(15.76950f, 31.67010f, 13.76950f, 31.95610f, 10.70580f, 32.13180f)
+                            curveTo(9.90f, 32.18690f, 9.53430f, 32.68730f, 9.53430f, 33.29790f)
+                            curveTo(9.53430f, 33.94750f, 10.02070f, 34.45680f, 10.71640f, 34.41780f)
+                            close()
+                        }
+                    }
+                }.build()
+        return _bookPages!!
+    }
+
+private var _bookPages: ImageVector? = null
